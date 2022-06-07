87434fe2aad14a9d8410ba5027da9a201ad7542b
Kacper Gawinkowski <s22738@pjwstk.edu.pl>
2022-06-07 09:49:32 +0200

    Nothing works, ez?

---
+++ APBD_10/APBD_10/Data/APBD_10Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APBD_10.Models;
+
+namespace APBD_10.Data
+{
+    public class APBD_10Context : DbContext
+    {
+        public APBD_10Context (DbContextOptions<APBD_10Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APBD_10.Models.Movie> Movie { get; set; }
+    }
+}
